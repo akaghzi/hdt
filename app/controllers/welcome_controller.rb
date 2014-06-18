858ddec887d9d871919e93452c41be418147023e
Asim Kaghzi <akaghzi@gmail.com>
2014-06-18 01:22:52 -0600

    added tasks scaffold

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,8 @@ class WelcomeController < ApplicationController
   skip_before_filter :authenticate_user!
   def index
     if user_signed_in?
-      @totalwishlistitems = ListItem.unbasketed.count
+      @totalwishlistitems = ListItem.unbasketed.where(user_id: current_user.id).count
+      @totaltaskitems = Task.incomplete.where(user_id: current_user.id).count
       @totalspending = ListItem.completed.where(user_id: current_user.id).sum("price")
     end
     # @totaltaskspending 
