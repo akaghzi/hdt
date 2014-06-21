7c701ad20fa0da64c480e631d2968b7602a260f9
Asim Kaghzi <akaghzi@gmail.com>
2014-06-21 12:58:45 -0600

    fixed minor stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation,
-                                                            :store_id) }
+                                                            :store_id, :brand_id, :unit_id, :item_category_id) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :password, :password_confirmation,
                                                                    :store_id, :brand_id, :unit_id, :current_password, 
                                                                    :item_category_id, :admin, :approved) }
