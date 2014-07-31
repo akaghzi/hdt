e7c33b9b0f5ca09503752681a5caee9d846c8c5c
Asim Kaghzi <akaghzi@gmail.com>
2014-07-31 16:19:20 -0600

    fixed formatting

--- app/controllers/last30days_controller.rb
+++ app/controllers/last30days_controller.rb
@@ -1,6 +1,7 @@
 class Last30daysController < ApplicationController
   def index
-    @last30daysbyitem = ListItem.completed.where(user_id: current_user.id).select("name, sum(price) as totalprice").group("name").order("totalprice desc")
+    @totalprice = 0.00
+    # @last30daysbyitem = ListItem.completed.where(user_id: current_user.id).select("name, sum(price) as totalprice").group("name").order("totalprice desc")
     @last30daysbystore = ListItem.completed.where(user_id: current_user.id).select("store_id, sum(price) as totalprice").group("store_id").order("totalprice desc")
     @last30daysbycategory = ListItem.completed.where(user_id: current_user.id).select("item_category_id, sum(price) as totalprice").group("item_category_id").order("totalprice desc")
     @last30daysbybrand = ListItem.completed.where(user_id: current_user.id).select("brand_id, sum(price) as totalprice").group("brand_id").order("totalprice desc")
